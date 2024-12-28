b7847bebf7ec7c5b085eab3cd468845097bd0828
cdkewin <danielchifa48@gmail.com>
2024-12-28 14:27:02 +0200

    successfully coded the overwriting of the GitHubProjects

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #Chifa Daniel Kewin Homework 1: Library Usage and Security Analysis
 import argparse
+import shutil
+import stat
 import subprocess
-
+import os
 
 
 #Taking the input link from the command line using the argparse module due to its automatic type conversion
@@ -17,20 +19,27 @@ def main():
     clone_repository(link)
     create_requirements_file("./ProjectsFromGithub")
 
+def handle_remove_readonly(func, path, exc_info):
+
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)  # Change the permission to writable
+        func(path)  # Retry the function after the event handling (os.rmdir) after gaining permission
+
 
 
 def clone_repository(github_url):
     destination_path = "./ProjectsFromGithub"
+    git_link = github_url
     try:
         print(f"Cloning repository from {github_url} into {destination_path}...")
         subprocess.run(["git", "clone", github_url, destination_path], check=True)
         print("Repository cloned successfully")
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}. Failed to clone repository.")
-
-
-
-
+        if os.path.exists(destination_path):  # Check if the directory exists
+            shutil.rmtree(destination_path, onerror=handle_remove_readonly)  # Handle permission errors to remove the files
+            print("Directory deleted successfully.")
+        clone_repository(git_link)  # Retry cloning
 
 
 def create_requirements_file(project_path):

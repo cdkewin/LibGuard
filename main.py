4d94df6225869aed3046f10420d68e87e6b1cd9d
cdkewin <danielchifa48@gmail.com>
2025-01-03 12:32:02 +0200

    optimized the used libraries from the environment and used pip freeze to create the optimized requirements file. fully removed the Project class and its uses. improved repo cloning function so it doesn't get stuck on an infinite loop

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 #Chifa Daniel Kewin Homework 1: Library Usage and Security Analysis
+
 import argparse
+import os
 import shutil
 import stat
 import subprocess
-import os
-import json
-
-class Project :
-    def __init__(self, libraries):
-        self.libraries = libraries
-    def getLibraries(self):
-        return self.libraries
+import sys
 
 
 #Taking the input link from the command line using the argparse module due to its automatic type conversion
@@ -23,32 +18,40 @@ def main():
     link = args.url #Storing the url attribute as 'link' for simplicity
 
 
-  #  clone_repository(link)
-   # create_requirements_file("./ProjectsFromGithub")
+    clone_repository(link)
+    create_requirements_file("./ProjectsFromGithub")
     display_imported_libraries_from_project(link)
     security_check()
 
 def handle_remove_readonly(func, path, exc_info):
 
     if not os.access(path, os.W_OK):
-        os.chmod(path, stat.S_IWUSR)  # Change the permission to writable
-        func(path)  # Retry the function after the event handling (os.rmdir) after gaining permission
+        os.chmod(path, stat.S_IWUSR)  #Change the permission to writable
+        func(path)  #Retry the function after the event handling (os.rmdir) after gaining permission
 
 
 
 def clone_repository(github_url):
-    destination_path = "./ProjectsFromGithub"
-    git_link = github_url
-    try:
-        print(f"Cloning repository from {github_url} into {destination_path}...")
-        subprocess.run(["git", "clone", github_url, destination_path], check=True)
-        print("Repository cloned successfully")
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}. Failed to clone repository.")
-        if os.path.exists(destination_path):  # Check if the directory exists
-            shutil.rmtree(destination_path, onerror=handle_remove_readonly)  # Handle permission errors to remove the files
-            print("Directory deleted successfully.")
-        clone_repository(git_link)  # Retry cloning
+    i = 0
+    if i < 3:  #After 3 attempts of cloning the repository the program will stop
+        destination_path = "./ProjectsFromGithub"
+        git_link = github_url
+
+        try:
+            print(f"Cloning repository from {github_url} into {destination_path}...")
+            subprocess.run(["git", "clone", github_url, destination_path], check=True)
+            print("Repository cloned successfully")
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e}. Failed to clone repository.")
+            if os.path.exists(destination_path):  #Check if the directory exists
+                shutil.rmtree(destination_path,
+                              onerror=handle_remove_readonly)  #Handle permission errors to remove the files
+                print("Directory deleted successfully.")
+                clone_repository(git_link)  #Retry cloning
+        i += 1
+    if i >= 3:
+        print("Cloning failed. Number of attempts: " + str(i))
+        sys.exit()
 
 
 def create_requirements_file(project_path):
@@ -75,29 +78,21 @@ def create_requirements_file(project_path):
 
 
 def display_imported_libraries_from_project(link) :
-    # Getting the name of the project for a nicer reading from the user
+    #Getting the name of the project for a nicer reading from the user
     project_name = link.split("/")[-1]
 
     print(f'\n\tThe imported libraries used in {project_name}\n')
     with open("./ProjectsFromGithub/requirements.txt", "r") as file:
-        # Using a set we store the libraries and remove copies
-        libraries = set(line.strip() for line in file if line.strip())  # Strip spaces and ignore empty lines
+        #Using a set we store the libraries and remove copies
+        libraries = set(line.strip() for line in file if line.strip())  #Strip spaces and ignore empty lines
 
-        # Print each library once
+        #Print each library once
         for library in libraries:
             print(library)
 
 
-def turn_project_to_object():    # This function will only be called from the security check function which will be called from main
-    with open("./ProjectsFromGithub/requirements.txt", "r") as file:
-        libraries = set(line.strip() for line in file if line.strip())
-    p = Project(libraries)
-    return p
-
 
 def security_check():
-    project = turn_project_to_object()
-    libraries = project.getLibraries()
 
     try:
         result = subprocess.run(
@@ -110,11 +105,11 @@ def security_check():
             print("No security issues found in the project.")
         else:
             print("Security issues detected:")
-            print(result.stdout)  # Print the detected issues
+            print(result.stdout)  #Print the detected issues
     except Exception as e:
         print(f"An error occurred: {e}")
 
-if __name__ == "__main__":  # Condition so the script doesn't run if imported as a module only if the main function is called explicitly
+if __name__ == "__main__":  #Condition so the script doesn't run if imported as a module only if the main function is called explicitly
     main()
 
 
